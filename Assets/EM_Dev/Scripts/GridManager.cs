8ec127ee6393f384f55cc6165fa38930c8bbb80c
Kim_EuiMyoung <aud1035@naver.com>
2024-08-07 12:37:10 +0900

    코드정리

--- Assets/EM_Dev/Scripts/GridManager.cs
+++ Assets/EM_Dev/Scripts/GridManager.cs
@@ -1,24 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
 
 public class GridManager : MonoBehaviour
 {
+    enum Type
+    {
+        Draw,
+        Text
+    }
     public GameObject cellPrefab;
     public int rows = 10;
     public int columns = 10;
+    [SerializeField]
+    Type cellType;
+
+    public TMP_InputField[,] inputFields;
 
     void Start()
     {
+        inputFields = new TMP_InputField[rows, columns];
         PopulateGrid();
     }
 
     void PopulateGrid()
     {
-        for (int i = 0; i < rows ; i++) 
+        for (int i = 0; i < rows; i++)
         {
-            for(int j = 0; j < columns ; j++)
-                Instantiate(cellPrefab, transform); // 按眉 积己蠢肠
+            for (int j = 0; j < columns; j++)
+            {
+                GameObject cell = Instantiate(cellPrefab, transform); // 按眉 积己
+
+            }
         }
     }
 }
