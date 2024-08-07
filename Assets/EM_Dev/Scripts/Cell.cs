430dc5db7f233d74ec911432e58423cf7fad933c
Kim_EuiMyoung <aud1035@naver.com>
2024-08-07 10:16:33 +0900

    마우스 드래그 및 한칸 지우개 구현

--- Assets/EM_Dev/Scripts/Cell.cs
+++ Assets/EM_Dev/Scripts/Cell.cs
@@ -1,31 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 
-public class Cell : MonoBehaviour
+public class Cell : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
     private Image cellImage;
     private Color originalColor;
     private Color prevColor;
     private ButtonManager buttonManager;
 
+    private bool isDragging = false;
+    private Color changeColor;
+
     void Start()
     {
         cellImage = GetComponent<Image>();
         originalColor = Color.white;
         buttonManager = FindObjectOfType<ButtonManager>();
-        GetComponent<Button>().onClick.AddListener(OnCellClicked);
+        //GetComponent<Button>().onClick.AddListener(OnCellClicked);
     }
 
     void OnCellClicked()
     {
-        Color changeColor = buttonManager.GetSelectedColor();
+        changeColor = buttonManager.GetSelectedColor();
         changeColor.a = 1;
         
         
         cellImage.color = cellImage.color == changeColor ? originalColor : changeColor; 
 
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isDragging = true;
+        changeColor = buttonManager.GetSelectedColor();
+        changeColor.a = 1;
+        UpdateCellColor(eventData);
+
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if(isDragging)
+        {
+            UpdateCellColor(eventData);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isDragging= false;
+    }
+    private void UpdateCellColor(PointerEventData eventData)
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            transform.parent.GetComponent<RectTransform>(),
+            eventData.position,
+            eventData.pressEventCamera,
+            out Vector2 localPoint
+        );
+
+        foreach (Transform child in transform.parent)
+        {
+            RectTransform rectTransform = child.GetComponent<RectTransform>();
+            if (rectTransform.rect.Contains(localPoint - (Vector2)rectTransform.localPosition))
+            {
+                Image childImage = child.GetComponent<Image>();
+                childImage.color = changeColor;
+
+                //childImage.color = childImage.color == changeColor ? originalColor : changeColor;
+            }
+        }
     }
 }
